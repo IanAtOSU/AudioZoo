5eee1c6e043a669d6c80ecc8888d6a07e7ef0b9b
TAssassinIT <81654918+TAssassinIT@users.noreply.github.com>
2023-02-01 12:57:27 -0800

    Click/drag difference + selected sprite variable

--- main.py
+++ main.py
@@ -16,19 +16,21 @@ sprites = [sprite.sprite("Sprites/sprite0.gif", "Sounds/bruh.mp3"), sprite.sprit
 dragging = False
 initmousepos=[0,0]#initial position of mouse when clicking on sprite, used to calculate where the sprite should be
 initspritepos=[0,0]#initial position of sprite when clicking on sprite
+mouse_x = 0
+mouse_y = 0
 #position sprites on screen.
 
 selected_sprite = sprites[0]
 
 def check_for_drag(): 
     global dragging, mouse_x, mouse_y, initmousepos, initspritepos, sprites
+    tmp = None
     for i in range(len(sprites)-1,-1,-1):#play corresponding sound to sprite clicked on, prioritize sprites displayed last/on top
         if sprites[i].rect.collidepoint(pygame.mouse.get_pos()):
             mouse_x,mouse_y=event.pos
             dragging = True
             initmousepos=[mouse_x,mouse_y]
             initspritepos=[sprites[i].rect.x,sprites[i].rect.y]
-            pygame.mixer.Sound(sprites[i].get_mod_sound()).play()
             tmp=sprites[i] #give the object clicked on top priority
             sprites.remove(tmp)
             sprites.append(tmp)
@@ -43,6 +45,11 @@ while True:
             selected_sprite = check_for_drag()
         elif event.type == pygame.MOUSEBUTTONUP:
             dragging = False
+            if abs(mouse_x-initmousepos[0]) < 5 and abs(mouse_y-initmousepos[1]) < 5 and selected_sprite != None:
+                pygame.mixer.Sound(sprites[i].get_mod_sound()).play()
+                sprites[len(sprites)-1].rect.x = initspritepos[0]
+                sprites[len(sprites)-1].rect.y = initspritepos[1] 
+            
         elif event.type == pygame.MOUSEMOTION and dragging:
             mouse_x,mouse_y = event.pos
             sprites[len(sprites)-1].rect.x = initspritepos[0]+mouse_x-initmousepos[0]#object being dragged is always the last one
